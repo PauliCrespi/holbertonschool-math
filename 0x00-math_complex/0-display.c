c1ab76deea900a6326975d13b80953b0f9f26f6d
PauliCrespi <4377@holbertonschool.com>
2022-03-18 06:23:47 -0700

    task 0

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *display_complex_number - function
+ *@c : complex
+ */
+void display_complex_number(complex c)
+{
+	if (c.re != 0)
+		printf("%g", c.re);
+	if (c.im != 0)
+		printf(" + %gi\n", c.im);
+	else
+		printf("\n");
+}
