ddf17743098ee05d932180a207d00c35a7d156c9
PauliCrespi <4377@holbertonschool.com>
2022-03-18 06:27:57 -0700

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,8 +7,10 @@ void display_complex_number(complex c)
 {
 	if (c.re != 0)
 		printf("%g", c.re);
+	if (c.re != 0 && c.im != 0)
+		printf(" + ");
 	if (c.im != 0)
-		printf(" + %gi\n", c.im);
+		printf("%gi\n", c.im);
 	else
 		printf("\n");
 }
