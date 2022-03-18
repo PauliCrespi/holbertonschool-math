a9e8a25704ab88c08c9ea98cc74c19ffe32ff717
PauliCrespi <4377@holbertonschool.com>
2022-03-18 07:23:45 -0700

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -5,21 +5,32 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re >= 0)
+	if (!c.re && !c.im)
+		printf("0\n");
+
+	if (c.re != 0)
 		printf("%g", c.re);
-	else if (c.re < 0)
-		printf("-%g", c.re);
-	if (c.im != 0)
+
+	if (c.im > 0 && c.re != 0)
 	{
+		printf(" + ");
+
 		if (c.im == 1)
-			printf(" + i\n");
-		else if (c.im == -1)
-			printf(" - i\n");
-		else if (c.im < 0)
-			printf(" - %gi\n", c.im);
-		else if (c.im > 0)
-			printf(" + %gi\n", c.im);
+			printf("i\n");
+		else
+			printf("%gi\n", c.im);
 	}
-	else if (c.re != 0 && c.im == 0)
+
+	if (c.im < 0 && c.re != 0)
+	{
+		printf(" - ");
+
+		if (c.im == -1)
+			printf("i\n");
+		else
+			printf("%gi\n", c.im * -1);
+	}
+
+	if (c.im == 0)
 		printf("\n");
 }
